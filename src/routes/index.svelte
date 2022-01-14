bbad1e4d7604acadb3d0481e50153698621adb3f
John Sim <jsim@fishbowlsolutions.com>
2022-01-14 18:27:43 +0000

    add moralis support to login and grab users NFTs

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,11 +1,16 @@
 
 <script>
+	import { onMount } from 'svelte';
+
+  import {flip} from 'svelte/animate';
+  import {dndzone} from 'svelte-dnd-action';
+
   import ogSquareImageSrc from '$lib/assets/home/home-open-graph-square.jpg';
   import ogImageSrc from '$lib/assets/home/home-open-graph.jpg';
   import twitterImageSrc from '$lib/assets/home/home-twitter.jpg';
   import featuredImageSrc from '$lib/assets/home/home.jpg';
-  import BlogRoll from '$lib/components/BlogRoll.svelte';
-  import Card from '$lib/components/Card.svelte';
+  //import BlogRoll from '$lib/components/BlogRoll.svelte';
+  //import Card from '$lib/components/Card.svelte';
   import SEO from '$lib/components/SEO/index.svelte';
   import website from '$lib/config/website';
 
@@ -59,14 +64,159 @@
     ogSquareImage,
     twitterImage,
   };
+
+  
+  //let Moralis;
+  /* Moralis init code */
+  const serverUrl = 'https://up4dw9h6ggww.usemoralis.com:2053/server';
+  const appId = 'vq18qD9kJiueOdtWq6qzQXPwTCuRpHRCjxYD0sRh';
+
+  $:userNFTs = [];
+
+  onMount(async() => {
+    //Moralis = await import('moralis/dist/moralis.min.js');
+    
+    await Moralis.start({ serverUrl, appId });
+    //autologin
+    login();
+
+  });
+
+  /**
+   * login
+   */
+  async function login() {
+    console.log('[login]')
+    let user = Moralis.User.current();
+    console.log('[login]',user)
+    if (!user) {
+      user = await Moralis.authenticate({ signingMessage: "Log in to The IMR Bridge via the Moralis SDK" })
+        .then(function (user) {
+          console.log("logged in user:", user);
+          console.log(user.get("ethAddress"));
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+    }
+  }
+  
+  /**
+   * logOut
+   */
+  async function logOut() {
+    await Moralis.User.logOut();
+    console.log("logged out");
+  }
+
+  /**
+   * getNFTs
+   */
+  async function getNFTs() {
+    const userEthNFTs = await Moralis.Web3API.account.getNFTs({ chain: 'rinkeby' });
+    console.log('userEthNFTs',userEthNFTs.result);
+    const hasNFTs = userEthNFTs.result.filter((nft) => {
+      if (nft.metadata !== null) {
+        nft.metadata = JSON.parse(nft.metadata);
+      }
+      return (nft.metadata !== null);
+    });
+    console.log('hasNFTs',hasNFTs)
+    userNFTs = hasNFTs;
+  }
+
+
+  /**
+   * getImg
+   */
+  function getImg(path) {
+    if (path.startsWith('ipfs')) {
+        let splitPath = path.replace(/ipfs:\/\//g,'').split('/');
+        //console.log(splitPath);
+        path = `https://${splitPath[0]}.ipfs.infura-ipfs.io/${splitPath[1]}`
+    }
+
+    return path;
+  }
 </script>
 
-<style lang="scss">
+<style>
+  .interlinkedItems {
+    margin:0px;
+    padding:4px 8px;
+    border:solid 1px #ccc;
+    margin-bottom:20px;
+    border-radius: 8px;
+  }
+  .interlinkedItems li {
+    display:flex;
+    list-style: none;
+    margin:0px;
+  }
+  .interlinkedItems li > div {
+    flex:1;
+    border:solid 1px #ccc;
+    border-radius: 8px;
+    margin:4px 8px;
+    overflow: hidden;
+    padding:10px;
+  }
+  .interactionOptions {
+    display: flex;
+  }
+  .interactionOptions select {
+    flex:1;
+  }
+  .header {
+    text-align:center;
+    font-weight:bold;
+  }
 </style>
 
 
 <SEO {...seoProps} />
+
+
+<button on:click="{login}">login</button>
+<button on:click="{logOut}">logout</button>
+<button on:click="{getNFTs}">getNFTs</button>
+
+
 <header>
   <h1>NFT Immersive Reality Bridge</h1>
   <h2>dApp Written on top of Svelte, Moralis</h2>
 </header>
+
+<div>
+  <ul class="interlinkedItems">
+    <li class="header">
+      <div>Flat NFT (Image)</div>
+      <div>Interaction</div>
+      <div>3D NFT (GLB)</div>
+    </li>
+    <li>
+      <div></div>
+      <div class="interactionOptions">
+        <select>
+          <option>Auto</option>
+          <option>Touch</option>
+        </select>
+      </div>
+      <div></div>
+    </li>
+  </ul>
+  <div class="actions">
+      <button class="create" on:click="{() => { }}">
+           + Create New Interlinked NFT 
+      </button>
+  </div>
+</div>
+
+<ul>
+  {#each userNFTs as nft}
+    <li>
+      <img src="{getImg(nft.metadata.image)}" alt=""/>
+      {nft.metadata.name}
+    </li>
+  {/each}
+</ul>
