c78b25a235963779f0cf1815c158ad99e35cf8c2
John Sim <jsim@fishbowlsolutions.com>
2022-01-15 13:12:27 +0000

    try again

--- svelte.config.js
+++ svelte.config.js
@@ -4,7 +4,6 @@ import adapter from '@sveltejs/adapter-netlify';
 import { imagetools } from 'vite-imagetools';
 import { mdsvex } from 'mdsvex';
 import preprocess from 'svelte-preprocess';
-import nodePolyfills from 'rollup-plugin-polyfill-node';
 
 const config = {
   extensions: ['.svelte', '.md', '.svelte.md'],
@@ -26,20 +25,8 @@ const config = {
     vite: {
       define: {
         'process.env.VITE_BUILD_TIME': JSON.stringify(new Date().toISOString()),
-        global: {}
       },
-      plugins: [
-        imagetools({ force: true }),
-        nodePolyfills({
-          include: [
-            '*.js',
-           'node_modules/**/*.js',
-            new RegExp('node_modules/.vite/.*js')
-          ],
-          // ↓ Not sure if this line is necessary, seems to work without it
-          exclude: ['node_modules/polyfill-nodeglobal.js']
-        })
-      ],
+      plugins: [imagetools({ force: true })],
     },
   },
 };
